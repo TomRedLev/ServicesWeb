73904efc765666e7e2d96fbc9bd42f9b7ad92d3c
Tom Redon <redon.tom@gmail.com>
2021-11-25 08:43:58 +0100

    Added some ideas of amelioration

--- RMI/src/fr/eiffelcorp/ifshare/rmi/server/IfShareServer.java
+++ RMI/src/fr/eiffelcorp/ifshare/rmi/server/IfShareServer.java
@@ -8,6 +8,16 @@ import fr.eiffelcorp.ifshare.rmi.common.IShop;
 
 // To stop it : sudo kill -9 $(sudo lsof -t -i:8081)
 
+// Ameliorations : 
+	// Ajouter une fonction qui envoie un Token unique (permettra d'authentifier les transactions d'ajout et de delete).
+	// Modifier la fonction de delete pour demander le token et le nom de l'utilisateur (double vérification).
+	//Ajouter le champ Token dans les products.
+	// On peut créer une hashmap qui contiendra token : nom, pour ne pas régénérer un token pour une personne qui s'est
+	// déjà connectée.
+	// Permettra de ne demander que le token de la personne et de vérifier si le nom est bien commun.
+	// Si on fait ça, plus besoin du token dans les products.
+	
+
 public class IfShareServer {
 	public static void main(String[] args) {
 		try {
